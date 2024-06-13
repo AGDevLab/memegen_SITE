98c8d6dc0df38d6b8de15705f609bbab358c6ba8
Amit Gal <amitgal@hotmail.com>
2024-06-13 19:53:04 +0300

    2nd Submission

--- js/meme.controller.js
+++ js/meme.controller.js
@@ -1,5 +1,15 @@
 'use strict'
 
+function onInit() {
+  gElCanvas = document.querySelector('canvas')
+  gCtx = gElCanvas.getContext('2d')
+  console.log('gCtx:', gCtx)
+  // renderMeme()
+
+  resizeCanvas()
+  window.addEventListener('resize', resizeCanvas)
+}
+
 function onSelectImg(elImg) {
   coverCanvasWithImg(elImg)
 }
