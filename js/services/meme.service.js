436df74b0d9f5dd1a81c6e7f7a880a8eb7af35a0
Amit Gal <amitgal@hotmail.com>
2024-06-13 13:36:41 +0300

    First Sub

--- js/services/meme.service.js
+++ js/services/meme.service.js
@@ -4,12 +4,13 @@ function coverCanvasWithImg(elImg) {
   gElCanvas.height =
     (elImg.naturalHeight / elImg.naturalWidth) * gElCanvas.width
   gCtx.drawImage(elImg, 0, 0, gElCanvas.width, gElCanvas.height)
+  drawText('placeHolder Text test', gElCanvas.width / 2, gElCanvas.height / 2)
 }
 
 function drawText(text, x, y) {
   gCtx.lineWidth = 2
-  gCtx.strokeStyle = 'brown'
-  gCtx.fillStyle = 'black'
+  gCtx.strokeStyle = 'green'
+  gCtx.fillStyle = 'white'
   gCtx.font = '40px Arial'
   gCtx.textAlign = 'center'
   gCtx.textBaseline = 'middle'
