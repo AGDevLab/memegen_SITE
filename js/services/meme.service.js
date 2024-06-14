7542748138bd2a41a4233f36e0f47c291ade169e
Amit Gal <amitgal@hotmail.com>
2024-06-14 03:52:31 +0300

    Pre fix render code

--- js/services/meme.service.js
+++ js/services/meme.service.js
@@ -3,20 +3,72 @@
 var gElCanvas
 var gCtx
 
+var gImgs = [{ id: 1, url: 'img/1.jpg', keywords: ['funny', 'cat'] }]
+var gMeme = {
+  selectedImgId: 5,
+  selectedLineIdx: 0,
+  lines: [
+    {
+      txt: 'I sometimes eat Falafel',
+      size: 20,
+      color: 'red',
+    },
+  ],
+}
+var gKeywordSearchCountMap = { funny: 12, cat: 16, baby: 2 }
+
 // function renderMeme() {
 //   gElCanvas = document.querySelector('canvas')
 //   gCtx = gElCanvas.getContext('2d')
 //   console.log('gCtx:', gCtx)
 // }
+// console.log(gMeme)
+// console.log(gMeme.lines)
 
-function renderMeme(src = 'img/1.jpg', msgInput) {
+// var gMemeText = gMeme.lines
+// console.log(gMemeText[0].size)
+
+function getMeme() {
+  // const memeInfo = gMeme.lines
+  // const src = elImg.src
+  // console.log(src)
+
+  // // const msgInput = prompt('input Text')
+  // const msgInput = memeInfo[0].txt
+  // renderMeme(src, msgInput)
+
+  return gMeme
+}
+
+function renderMeme(elGalleryImg) {
+  const meme = getMeme()
+  const memeInfo = meme.lines
+  const msgInput = memeInfo[0].txt
+
+  const src = elGalleryImg.src
   const showEditor = document.querySelector('.editor')
-  console.log(showEditor)
+  // console.log(showEditor)
   showEditor.showModal()
   const elImg = new Image()
   elImg.src = src
   coverCanvasWithImg(elImg)
   drawText(msgInput, gElCanvas.width / 2, gElCanvas.height / 2)
+}
+
+function setLineTxt() {
+  // Get the input element by its ID
+  let inputField = document.getElementById('myInput')
+  const meme = getMeme()
+  const memeInfo = meme.lines
+  // const msgInput = memeInfo[0].txt
+
+  // Get the value of the input field
+  memeInfo[0].txt = inputField.value
+  console.log(memeInfo[0].txt)
+
+  // renderMeme()
+
+  // return value
 }
 
 function coverCanvasWithImg(elImg) {
